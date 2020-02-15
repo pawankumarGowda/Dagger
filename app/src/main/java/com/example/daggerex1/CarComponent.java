43ac95aeb6934fb8b66a0a4be6ffa1d0bd2a10bd
pawan <pawan06.mac@gmail.con>
2020-02-15 14:03:11 +0530

    DaggerExampleVersion1

---
+++ app/src/main/java/com/example/daggerex1/CarComponent.java
@@ -0,0 +1,9 @@
+package com.example.daggerex1;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+
+    Car getCar();
+}
---
+++ app/src/main/java/com/example/daggerex1/Engine.java
@@ -0,0 +1,9 @@
+package com.example.daggerex1;
+
+import javax.inject.Inject;
+
+public class Engine {
+    @Inject
+    public Engine() {
+    }
+}
